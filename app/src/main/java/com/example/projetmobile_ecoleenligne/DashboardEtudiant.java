81070a57be81ff04f1ae4ab4a0236d60ca6a42c1
sabrine <sabrinefarah22@gmail.com>
2022-04-09 14:09:38 +0200

    Initial commit

---
+++ app/src/main/java/com/example/projetmobile_ecoleenligne/DashboardEtudiant.java
@@ -0,0 +1,13 @@
+package com.example.projetmobile_ecoleenligne;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DashboardEtudiant extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard_etudiant);
+    }
+}
---
+++ app/src/main/java/com/example/projetmobile_ecoleenligne/Intro.java
@@ -0,0 +1,13 @@
+package com.example.projetmobile_ecoleenligne;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Intro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
